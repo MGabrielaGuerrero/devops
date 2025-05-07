6aaa7325c6c9461195966e304454bb423a20fa1c
MGabrielaGuerrero <ggmaria@unicauca.edu.co>
2025-05-07 18:34:19 -0500

    Add RDS

--- cdk/lib/stack.ts
+++ cdk/lib/stack.ts
@@ -1,25 +1,72 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class Stack extends cdk.Stack {
+  readonly vpc: ec2.Vpc;
+  readonly dbInstance: rds.DatabaseInstance;
+  readonly dbSecret: secretsmanager.Secret;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // VPC con subredes públicas y privadas
-    const vpc = new ec2.Vpc(this, 'MyVPC', {
+    this.vpc = new ec2.Vpc(this, 'BlossomVPC', {
       maxAzs: 2,
       natGateways: 1,
       subnetConfiguration: [
         {
           name: 'Public',
           subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
         },
         {
           name: 'Private',
           subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
         },
       ],
     });
+
+    // Secreto con credenciales de PostgreSQL
+    this.dbSecret = new secretsmanager.Secret(this, 'DBCredentialsSecret', {
+      secretName: 'lab-db-credentials',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: 'myusername' }),
+        generateStringKey: 'password',
+        excludePunctuation: true,
+      },
+    });
+
+    // Grupo de seguridad para RDS
+    const dbSecurityGroup = new ec2.SecurityGroup(this, 'PostgresSG', {
+      vpc: this.vpc,
+      description: 'Permitir acceso al backend desde ECS',
+      allowAllOutbound: true,
+    });
+
+    // Instancia RDS PostgreSQL
+    this.dbInstance = new rds.DatabaseInstance(this, 'PostgresDB', {
+      engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_15 }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      vpc: this.vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      securityGroups: [dbSecurityGroup],
+      credentials: rds.Credentials.fromSecret(this.dbSecret),
+      databaseName: 'test_local',
+      allocatedStorage: 20,
+      maxAllocatedStorage: 100,
+      multiAz: false,
+      publiclyAccessible: false,
+      backupRetention: cdk.Duration.days(0),
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+    });
   }
 }
+
